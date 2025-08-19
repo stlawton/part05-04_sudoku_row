e361e5c03f0a55dfc62bd5f0b3b2a7c499d171b6
stlawton <lawtons347@gmail.com>
2025-08-19 19:45:14 -0400

    Final code

--- src/sudoku_row.py
+++ src/sudoku_row.py
@@ -1,14 +1,10 @@
 def row_correct(sudoku: list, row_no: int):
   row_size = len(sudoku[row_no]) # Store row size in variable
-  checked_numbers = [] # Empty list to store numbers that have already been checked
   number = 1
   while number <= row_size: # Iterate across row
     if number in sudoku[row_no]:
-      if checked_numbers.count(number) > 0:
+      if sudoku[row_no].count(number) > 1:
         return False
-      else:
-        checked_numbers.append(number)
-      number += 1
-    else:
-      return False
+    number += 1
   return True
+
